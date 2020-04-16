08457e995a3f9d42f1981d434edd7a6598a737b8
David Headrick <david.headrick9@gmail.com>
2020-04-15 22:52:13 -0700

    added OpenGL audio spectrum

--- aoanalyser.cpp
+++ aoanalyser.cpp
@@ -16,10 +16,14 @@
 #include <pulse/simple.h>
 #include <pulse/error.h>
 #define BUFSIZE 1024
+//GLUT - window
+#include<GL/glut.h>
+#include <GLFW/glfw3.h>
 //matplotlib
 #include "matplotlibcpp.h"
 
 //g++ aoanalyser.cpp -I/usr/include/python2.7 -I/home/daviddjh/.local/lib/python2.7/site-packages/numpy/core/include/ -lfftw3 -lm -lpulse-simple -lpulse -g -std=c++11 -lpython2.7 
+// g++ aoanalyser.cpp -I/usr/include/python2.7 -I/home/daviddjh/.local/lib/python2.7/site-packages/numpy/core/include/ -lfftw3 -lfftw3f -lm -lpulse-simple -lpulse -g -std=c++11 -lpython2.7
 
 using namespace std;
 
@@ -27,23 +31,52 @@ namespace plt = matplotlibcpp;
 
 static ssize_t loop_write(int fd, const void*data, size_t size);
 
+void setupGlut(int* argc, char** argv);
+
+void Grender(void);
+
+
 int main (int argc, char *argv[]){
 
+    //setupGlut(&argc, argv);
+    GLFWwindow* window;
+    float r, g, b = 0;
 
-    fftw_complex *out;
-    double *in;
-    fftw_plan p;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1920, 1080, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    r = .11;
+    b = 0.6;
+    g = 0.01;
+
+
+
+    fftwf_complex *out;
+    //double *in;
+    float *in;
+    fftwf_plan p;
     int ret;
     float buf[BUFSIZE];
 
     //fftw arrays
     //in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * BUFSIZE);
-    in = (double*) fftw_malloc(sizeof(double) * BUFSIZE);
-    out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * BUFSIZE);
+    in = (float*) fftwf_malloc(sizeof(double) * BUFSIZE);
+    out = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * BUFSIZE);
 
     //fftw plan - setup for the transform to be executed
     //p = fftw_plan_dft_1d(BUFSIZE, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-    p = fftw_plan_dft_r2c_1d(BUFSIZE, in, out, FFTW_ESTIMATE);
+    p = fftwf_plan_dft_r2c_1d(BUFSIZE, in, out, FFTW_ESTIMATE);
 
     /* The sample type to use */
     static const pa_sample_spec ss = {
@@ -72,51 +105,103 @@ int main (int argc, char *argv[]){
     }
     */
 
-    float bass, mid, high;
-    for (int j = 0; j < 1000; j++){
-        pa_simple_read(s, buf, sizeof(buf), &error);
+    float bass, mid, high, midval;
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+       // for (int j = 0; j < 1000; j++){
+            pa_simple_read(s, buf, sizeof(buf), &error);
 
-        //fill fftw array with audio buffer
-        for(int i = 0; i < BUFSIZE; i++){
-            double multiplier = 0.5 * (1 - cos(2*3.1415*i/(BUFSIZE-1)));
-            in[i] = (multiplier * buf[i]);
+            //fill fftw array with audio buffer
+            for(int i = 0; i < BUFSIZE; i++){
+                double multiplier = 0.5 * (1 - cos(2*3.1415*i/(BUFSIZE-1)));
+                in[i] = (multiplier * buf[i]);
+            }
+            //in = buf;
+
+            fftwf_execute(p);
+
+            // Bass - 20 - 300      [1,7]   .1 = 35
+            // Mid  - 301 - 1280    [8,29]  .1 = 105
+            // High - 1281 - 10200  [30,233].1 = 1015
+            bass = 0;
+            mid = 0;
+            high = 0;
+
+
+            for(int i = 1; i < 234; i++){
+                if(i <= 7){
+                    bass += sqrt((out[i][0] * out[i][0]) + (out[i][1] * out[i][1])); 
+                }
+                if(i > 7 && i <= 29){
+                    mid += sqrt((out[i][0] * out[i][0]) + (out[i][1] * out[i][1])); 
+                }
+                if(i > 29 && i <= 233){
+                    high += sqrt((out[i][0] * out[i][0]) + (out[i][1] * out[i][1])); 
+                }
+            }   
+
+            system("clear");
+            cout << "Bass: " << pow((bass), 2) << endl;
+            cout << "Mid:  " << pow((mid), 2) << endl;
+            cout << "High: " << pow((high), 2) << endl << endl;
+            if(pow(bass, 2) > 21000){
+                cout << " +++ " << endl;
+            }
+
+        //}
+
+        b = bass/60;
+        g = mid/60;
+        r = high/60;
+
+        if(b < g < r || r < g < b){
+            midval = g;
+        } else if(g < b < r || r < b < g){
+            midval = b;
+        } else {
+            midval = r;
         }
-        //in = buf;
 
-        fftw_execute(p);
+        b=4*pow((b-(midval)),3)+(midval);
+        g=4*pow((g-(midval)),3)+(midval);
+        r=4*pow((r-(midval)),3)+(midval);
 
-        // Bass - 20 - 300      [1,7]   .1 = 35
-        // Mid  - 301 - 1280    [8,29]  .1 = 105
-        // High - 1281 - 10200  [30,233].1 = 1015
-        bass = 0;
-        mid = 0;
-        high = 0;
-
-
-        for(int i = 1; i < 234; i++){
-            if(i <= 7){
-                bass += sqrt((out[i][0] * out[i][0]) + (out[i][1] * out[i][1])); 
-            }
-            if(i > 7 && i <= 29){
-                mid += sqrt((out[i][0] * out[i][0]) + (out[i][1] * out[i][1])); 
-            }
-            if(i > 29 && i <= 233){
-                high += sqrt((out[i][0] * out[i][0]) + (out[i][1] * out[i][1])); 
-            }
-        }   
-
-        system("clear");
-        cout << "Bass: " << pow((bass), 2) << endl;
-        cout << "Mid:  " << pow((mid), 2) << endl;
-        cout << "High: " << pow((high), 2) << endl << endl;
-        if(pow(bass, 2) > 20000){
-            cout << " +++ " << endl;
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+        //glColor3f(r, 0, b);
+        //glColor3f(0, 0, b);
+        //glColor3f(0, 0, b);
+        float size;
+        //glRectf(-1.0f, -1.0f, 1.0, 1.0);  //start pos 
+        glColor3f(0.5, 0, b);
+        for(int k = 1; k < 200; k++){
+            size = sqrt((out[k][0] * out[k][0]) + (out[k][1] * out[k][1]));
+            glRectf(((float)2*k/200)-1, -1.0f, (((float)2*k/200)-1+(0.005f)), (size/40)-1);  //start pos 
+            /*
+            glRectf((2*k/233)-1, -1.0f,  //start pos 
+                    (2*k/233)-1, size);   //end pos
+                    *///
         }
+        /*
+        glBegin(GL_TRIANGLES);
+            glVertex2f(-1, -1);
+            glVertex2f(-1, 1);
+            glVertex2f(1, -1);
+            glVertex2f(1, 1);
+        glEnd();
+        */
 
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
 
+        /* Poll for and process events */
+        glfwPollEvents();
     }
 
+    glfwTerminate();
 
+    /*
     
     //Plot data on matlibplot graph
     double hz = 0;
@@ -136,12 +221,14 @@ int main (int argc, char *argv[]){
     //plt::plot(xplot, yplot, "b-"); 
     plt::plot( xplot, yplot, "b-"); 
     plt::show();
+    */
 
     ret = 0;
 
-    fftw_destroy_plan(p);
-    fftw_free(in);
-    fftw_free(out);
+    fftwf_destroy_plan(p);
+    fftwf_free(in);
+    fftwf_free(out);
+
 
 finish:
     if (s)
@@ -163,4 +250,33 @@ static ssize_t loop_write(int fd, const void*data, size_t size) {
         size -= (size_t) r;
     }
     return ret;
+}
+
+void setupGlut(int* argc, char** argv){
+    glutInit(argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(640,480);
+    glutCreateWindow("Simple GLut");
+
+    glutDisplayFunc(Grender);
+
+    glutMainLoop();
+}
+
+void Grender(){
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+        glColor3f(1, 0, 0);
+        glVertex2f(-0.5, -0.5);
+        glColor3f(0, 1, 0);
+        glVertex2f(0.5, -0.5);
+        glColor3f(0, 0, 1);
+        glVertex2f(0.0, 0.5);
+    glEnd();
+
+    glutSwapBuffers();
+
 }
